18ef92d208f813aeccffaba81295643f4112720f
X99ror <144711429+X99ror@users.noreply.github.com>
2025-06-22 18:50:39 +0530

    Data validation completed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.entity.config_entity import DataValidationConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig
@@ -15,7 +17,15 @@ if __name__=='__main__':
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(data_ingestion_artifact)
+        data_validation_config=DataValidationConfig(training_pipeline)
+        data_validation=DataValidation(
+            data_ingestion_artifact=data_ingestion_artifact,
+            data_validation_config=data_validation_config
+            )
         
+        logging.info("Initiating the data validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("Data validation Complete")
         
     except Exception as e:
         raise NetworkSecurityException(e,sys)

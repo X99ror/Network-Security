952419fefe4c59869287c37b19bb188d052fa608
X99ror <144711429+X99ror@users.noreply.github.com>
2025-06-24 12:04:06 +0530

    Model trainer and implemented mlflow

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.entity.config_entity import DataTransformationConfig
 from networksecurity.components.data_transformation import DataTranformation
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import  ModelTrainerConfig
 import sys
 
 
@@ -38,6 +40,11 @@ if __name__=='__main__':
         data_transformation_artifact=data_transformation.initiate_dat_transformation()
         print(data_transformation_artifact)
         logging.info("data transformation completed")
+        logging.info("Model training started")
+        model_training_conffig=ModelTrainerConfig(training_pipeline)
+        model_training_artifact=ModelTrainer(data_transformation_artifact=data_transformation_artifact,model_trainer_config=model_training_conffig)
+        model_training_artifact.initiate_model_trainer()
+        logging.info("Model training artifact created")
         
         
     except Exception as e:
--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -11,6 +11,11 @@ FILE_NAME:str="NetworkData.csv"
 TRAIN_FILE_NAME:str="train.csv"
 TEST_FILE_NAME:str="test.csv"
 SCHEMA_FILE_PATH=os.path.join("data_schema","schema.yaml")
+SAVED_MODEL_DIR=os.path.join("saved_model")
+MODEL_FILE_NAME="model.pkl"
+
+
+
 
 """ Data ingestion constants"""
 
@@ -41,3 +46,10 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS:dict={
 DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
 
 DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
+
+"""  Model trainer constants """
+MODEL_TRAINER_DIR_NAME:str="model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR:str="trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME:str="model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE:float=0.6
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD:float=0.05
